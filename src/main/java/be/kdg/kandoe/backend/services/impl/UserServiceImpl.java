f7cedf19caa5a19efe2a82f8a0bed5c78a104c09
Yiua <yiuasam.tam@gmail.com>
2016-02-15 17:38:48 +0100

    pom + serviceImpl klassen toegevoegd

---
+++ src/main/java/be/kdg/kandoe/backend/services/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package be.kdg.kandoe.backend.services.impl;
+
+import be.kdg.kandoe.backend.services.api.UserService;
+
+/**
+ * Created by Yiua on 15/02/16.
+ */
+public class UserServiceImpl implements UserService {
+}
