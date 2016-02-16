852238ef3b8ca3b48abb4e93f09c12b410881522
jakupkdg <jakup.pepa@student.kdg.be>
2016-02-16 13:46:39 +0100

    theme properties toegevoegd

--- src/main/java/be/kdg/kandoe/backend/dom/Theme.java
+++ src/main/java/be/kdg/kandoe/backend/dom/Theme.java
@@ -1,5 +1,6 @@
 package be.kdg.kandoe.backend.dom;
 
+import be.kdg.kandoe.backend.dom.roles.Role;
 import org.springframework.hateoas.Identifiable;
 
 import javax.persistence.*;
@@ -29,7 +30,27 @@ public class Theme implements Serializable,Identifiable<Integer>{
     @Enumerated(EnumType.STRING)
     private GameMode gameMode;
 
-    
+    @OneToMany(targetEntity = Theme.class, cascade = CascadeType.ALL,fetch = FetchType.EAGER,mappedBy = "mainTheme")
+    private List<Theme> subThemes;
+
+    @ManyToOne(targetEntity = Theme.class, cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinColumn(name = "MainThemeId",nullable = true)
+    private Theme mainTheme;
+
+    @OneToMany(targetEntity = Card.class, cascade = CascadeType.ALL,fetch = FetchType.EAGER,mappedBy = "theme")
+    private List<Card> cards;
+
+    @ManyToOne(targetEntity = Organisation.class, cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinColumn(name="OrganisationId",nullable = false)
+    private Organisation organisation;
+
+    @OneToOne(targetEntity = Circle.class, cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinColumn(name="CircleId",nullable = false)
+    private Circle circle;
+
+    @OneToMany()
+    private List<Role> roles;
+
     @Override
     public Integer getId() {
         return themeId;
